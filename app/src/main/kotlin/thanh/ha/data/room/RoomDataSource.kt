53799049948e0c308e0b3bb3bbc15609c6452060
Thanh.Ha <thanhahvan1@gmail.com>
2018-11-22 15:22:58 +0700

    add basic resource config

--- app/src/main/kotlin/thanh/ha/data/room/RoomDataSource.kt
+++ app/src/main/kotlin/thanh/ha/data/room/RoomDataSource.kt
@@ -5,7 +5,7 @@ import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
 
-@Database(entities = [DefinitionEntity::class], version = 1)
+@Database(entities = [DefinitionEntity::class], version = 1,exportSchema = false)
 abstract class RoomDataSource : RoomDatabase() {
 
     abstract fun currencyDao(): RoomDefinitionDao
