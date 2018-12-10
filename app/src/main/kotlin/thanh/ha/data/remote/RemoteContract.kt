6ba1a580f5806fd70be00a21e6863ecb0d3f5074
Thanh.Ha <thanhahvan1@gmail.com>
2018-12-10 16:01:02 +0700

    update the secret endpoints

--- app/src/main/kotlin/thanh/ha/data/remote/RemoteContract.kt
+++ app/src/main/kotlin/thanh/ha/data/remote/RemoteContract.kt
@@ -9,7 +9,17 @@ class RemoteContract {
         private const val API_VERSION = "v0/"
         private const val DEFINE = "define"
         const val GET_WORD_END_POINT = API_VERSION + DEFINE
+        // define a word
         const val TERM = "term"
+
+        // fine a definition by id
+        const val DEFID = "defid"
+
+        // get a random definition
+        const val RANDOM = "random"
+
+        // page index of the definition list
+        const val PAGE = "page"
 
     }
 }
