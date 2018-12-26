523ac0f50571bb6a2401fa15b1ccd899e0dc6601
Thanh.Ha <thanhahvan1@gmail.com>
2018-12-26 20:41:58 +0700

    update UI

--- app/src/main/kotlin/thanh/ha/bus/BusMessage.kt
+++ app/src/main/kotlin/thanh/ha/bus/BusMessage.kt
@@ -1,5 +1,9 @@
 package thanh.ha.bus
 
+import thanh.ha.domain.DefinitionInfo
+
 class RxEvent {
     data class EventRecentSearch(val word: String)
+
+    data class EventSavedLocal(val word: DefinitionInfo)
 }
--- app/src/main/kotlin/thanh/ha/view/home/HomeFragment.kt
+++ app/src/main/kotlin/thanh/ha/view/home/HomeFragment.kt
@@ -4,24 +4,21 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.design.chip.Chip
-import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AnimationUtils
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.fragment_home.*
 import thanh.ha.R
 import thanh.ha.base.BaseFragment
 import thanh.ha.bus.RxBus
 import thanh.ha.bus.RxEvent
-import thanh.ha.ui.adapters.SavedDefAdapter
+import thanh.ha.domain.DefinitionInfo
 
 class HomeFragment : BaseFragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-    private lateinit var adapter: SavedDefAdapter
-    private lateinit var wordDisposable: Disposable
+    private lateinit var mHomeViewModel: HomeViewModel
+    private lateinit var mDisposable: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,51 +34,52 @@ class HomeFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initView()
         getLocalSaved()
         getRecentSearch()
     }
 
     private fun getRecentSearch() {
 
-        wordDisposable = RxBus
+        mDisposable = RxBus
                 .listen(RxEvent.EventRecentSearch::class.java)
                 .subscribe {
                     val chip = Chip(context)
                     chip.text = it.word
-                    chip.isClickable = true
-                    chip.isCheckable = false
-                    chipGroup.addView(chip as View)
+                    cg_recent_search.addView(chip as View)
+                }
+        mDisposable = RxBus
+                .listen(RxEvent.EventSavedLocal::class.java)
+                .subscribe {
+                    val chip = Chip(context)
+                    chip.text = it.word.word
+                    cg_recent_search.addView(chip as View)
                 }
     }
 
     private fun getLocalSaved() {
-        homeViewModel.getLocalDefinitions()?.observe(
+        mHomeViewModel.getLocalDefinitions()?.observe(
                 this,
                 Observer {
-                    adapter.updateInfo(it!!)
-                    rv_saved_def.smoothScrollToPosition(0)
-                    hideLoadingDialog()
+                    addLocalSavedToChip(it!!)
                 }
         )
     }
 
-    private fun initView() {
-        val layoutManager = LinearLayoutManager(context,
-                LinearLayoutManager.VERTICAL, false)
-        adapter = SavedDefAdapter(context, null)
-        rv_saved_def.layoutManager = layoutManager
-        rv_saved_def.adapter = adapter
-        val animation = AnimationUtils
-                .loadLayoutAnimation(context, R.anim.layout_animation_from_bottom)
-        rv_saved_def.layoutAnimation = animation
+    private fun addLocalSavedToChip(data: List<DefinitionInfo>) {
+        for (item in data) {
+            val chip = Chip(context)
+            chip.text = item.word
+            chip.isClickable = true
+            chip.isCheckable = false
+            cg_saved_def.addView(chip as View)
+        }
     }
 
     private fun initViewModel() {
-        homeViewModel = ViewModelProviders
+        mHomeViewModel = ViewModelProviders
                 .of(this)
                 .get(HomeViewModel::class.java)
-        homeViewModel
+        mHomeViewModel
                 .let {
                     lifecycle.addObserver(it)
                 }
@@ -89,6 +87,6 @@ class HomeFragment : BaseFragment() {
 
     override fun onDestroy() {
         super.onDestroy()
-        if (!wordDisposable.isDisposed) wordDisposable.dispose()
+        if (!mDisposable.isDisposed) mDisposable.dispose()
     }
 }
